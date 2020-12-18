6bf84a04800638a349f72b580ff7587ca9ce76f5
Brian Harvey <brian.harv3y@gmail.com>
2020-12-18 13:23:10 -0800

    add textInput to Chat screen

--- screens/Chat.js
+++ screens/Chat.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
-import KeyboardAvoid from '../components/KeyboardAvoid';
+import { View, Text, TextInput, StyleSheet, Button } from 'react-native';
 
 
 const Chat = props => {
   return (
     <View style={styles.screen}>
-      
       <Text>This is the Chat Screen</Text>
       <Button title="Back to Messages" onPress={() => {
         props.navigation.navigate('Messages')
       }}/>
-      <KeyboardAvoid />
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="write something"
+        />
+      </View>
     </View>
   )
 }
@@ -19,7 +22,21 @@ const Chat = props => {
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
+    flexDirection: 'column'
+  },
+  inputContainer: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    padding: 10
+  } ,
+  textInput: {
+    padding: 10,
+    marginBottom: 30,
+    backgroundColor: '#ccc',
+    borderStyle: 'solid',
+    borderRadius: 10,
+    borderColor: 'black'
   }
-});
+})
 
 export default Chat;
