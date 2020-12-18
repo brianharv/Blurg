9097316110677348a7120598da21a80a86819c6f
Brian Harvey <brian.harv3y@gmail.com>
2020-12-18 14:59:16 -0800

    add Login screen

--- screens/Login.js
+++ screens/Login.js
@@ -1,24 +1,104 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, Button, TextInput } from 'react-native';
+import firebaseSDK from 'firebase';
 
-const Login = props => {
-  return (
-    <View style={styles.screen}>
-      <Text>This is the Login Screen</Text>
-      <Button title="View Messages" onPress={() => {
-        props.navigation.navigate('Messages')
-      }}
-      />
-    </View>
-  )
+export default class Login extends React.Component {
+	static navigationOptions = {
+		title: 'Chat App'
+	};
+
+	state = {
+		name: 'Your Name',
+		email: 'test@test.com',
+		password: 'password',
+		avatar: ''
+	};
+
+	onPressLogin = async () => {
+		const user = {
+			name: this.state.name,
+			email: this.state.email,
+			password: this.state.password,
+			avatar: this.state.avatar
+		};
+
+		const response = firebaseSDK.login(
+			user,
+			this.loginSuccess,
+			this.loginFailed
+		);
+	};
+
+	loginSuccess = () => {
+		console.log('login successful, navigate to chat.');
+		this.props.navigation.navigate('Chat', {
+			name: this.state.name,
+			email: this.state.email,
+			avatar: this.state.avatar
+		});
+	};
+
+	loginFailed = () => {
+		alert('Login failure. Please tried again.');
+	};
+
+	onChangeTextEmail = email => this.setState({ email });
+	onChangeTextPassword = password => this.setState({ password });
+
+	render() {
+		return (
+			<View>
+				<Text style={styles.title}>Email:</Text>
+				<TextInput
+					style={styles.nameInput}
+					placeHolder="test3@gmail.com"
+					onChangeText={this.onChangeTextEmail}
+					value={this.state.email}
+				/>
+				<Text style={styles.title}>Password:</Text>
+				<TextInput
+					style={styles.nameInput}
+					onChangeText={this.onChangeTextPassword}
+					value={this.state.password}
+				/>
+				<Button
+					title="Login"
+					style={styles.buttonText}
+					onPress={this.onPressLogin}
+				/>
+
+				<Button
+					title="Signup"
+					style={styles.buttonText}
+					onPress={() => this.props.navigation.navigate('Signup')}
+				/>
+
+        <Button title="Start Talking" onPress={() => 
+        {props.navigation.navigate('Messages')}}
+        />
+
+
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-}
+	title: {
+		marginTop: 16,
+		marginLeft: 16,
+		fontSize: 16
+	},
+	nameInput: {
+		height: 16 * 2,
+		margin: 16,
+		paddingHorizontal: 16,
+		borderColor: '#111111',
+		borderWidth: 1,
+		fontSize: 16
+	},
+	buttonText: {
+		marginLeft: 16,
+		fontSize: 42
+	}
 });
-
-export default Login;
