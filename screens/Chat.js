848373b3a662f2e86e9fc978b3c23f46186814a4
Brian Harvey <brian.harv3y@gmail.com>
2020-12-18 14:37:24 -0800

    add GiftedChat

--- screens/Chat.js
+++ screens/Chat.js
@@ -1,42 +1,10 @@
 import React from 'react';
-import { View, Text, TextInput, StyleSheet, Button } from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat';
 
-
-const Chat = props => {
-  return (
-    <View style={styles.screen}>
-      <Text>This is the Chat Screen</Text>
-      <Button title="Back to Messages" onPress={() => {
-        props.navigation.navigate('Messages')
-      }}/>
-      <View style={styles.inputContainer}>
-        <TextInput
-          style={styles.textInput}
-          placeholder="write something"
-        />
-      </View>
-    </View>
-  )
+export default class Chat extends React.Component {
+  render() {
+    return <GiftedChat />;
+  }
 }
 
-const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-    flexDirection: 'column'
-  },
-  inputContainer: {
-    flex: 1,
-    justifyContent: 'flex-end',
-    padding: 10
-  } ,
-  textInput: {
-    padding: 10,
-    marginBottom: 30,
-    backgroundColor: '#ccc',
-    borderStyle: 'solid',
-    borderRadius: 10,
-    borderColor: 'black'
-  }
-})
 
-export default Chat;
