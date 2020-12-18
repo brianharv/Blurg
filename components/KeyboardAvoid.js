f9efbc56b7980cc42b433cc6258108e701e7781c
Brian Harvey <brian.harv3y@gmail.com>
2020-12-18 13:00:35 -0800

    debug text area

--- components/KeyboardAvoid.js
+++ components/KeyboardAvoid.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import { View, KeyboardAvoidingView, TextInput, StyleSheet, Platform, TouchableWithoutFeedback, Keyboard  } from 'react-native';
+import { View, KeyboardAvoidingView, TextInput, StyleSheet, TouchableWithoutFeedback, Keyboard  } from 'react-native';
 
 const KeyboardAvoid = () => {
   return (
     <KeyboardAvoidingView
-      behavior={Platform.OS == "ios" ? "padding" : "height"}
-      style={styles.container}
+      style={{position: 'absolute', left: 0, right: 0, bottom: 0}}
+      behavior="position"
     >
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
         <View style={styles.inner}>
-          <TextInput placeholder="write something" style={styles.textInput} />
+          <TextInput 
+            placeholder="write something" 
+            style={styles.textInput} 
+            returnKeyType="send"
+            />
         </View>
       </TouchableWithoutFeedback>
     </KeyboardAvoidingView>
@@ -18,20 +22,21 @@ const KeyboardAvoid = () => {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
   },
   inner: {
     padding: 24,
     flex: 1
   },
   textInput: {
+    flexDirection: 'column',
+    
     backgroundColor: 'rgba(255, 255, 255, 0.4)',
     width: '90%',
     borderColor: 'grey',
     borderRadius: 10,
     borderWidth: 2,
     padding: 10,
-    marginBottom: 10,
     borderBottomWidth: 1,
   }
 });
--- screens/Chat.js
+++ screens/Chat.js
@@ -6,13 +6,13 @@ import KeyboardAvoid from '../components/KeyboardAvoid';
 const Chat = props => {
   return (
     <View style={styles.screen}>
-        <Text>This is the Chat Screen</Text>
-        <Button title="Back to Messages" onPress={() => {
-          props.navigation.navigate('Messages')
-        }}/>
+      
+      <Text>This is the Chat Screen</Text>
+      <Button title="Back to Messages" onPress={() => {
+        props.navigation.navigate('Messages')
+      }}/>
       <KeyboardAvoid />
     </View>
-    
   )
 }
 
