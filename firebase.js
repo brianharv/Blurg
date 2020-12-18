75b5294ba75c4d46744948e5c2e404cac00be2ce
Brian Harvey <brian.harv3y@gmail.com>
2020-12-18 14:51:55 -0800

    add firebase

---
+++ firebase.js
@@ -0,0 +1,27 @@
+import firebase from 'firebase';
+
+class FirebaseSDK {
+  constructor() {
+    if (!firebase.app.length) {
+      firebase.initializeApp({
+        apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+        authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+        projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+        storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+        appId: process.env.REACT_APP_FIREBASE_APP_ID
+      })
+    }
+  }
+  login = async (user, success_callback, failed_callback) => {
+    await firebase
+      .auth()
+      .signInWithEmailAndPassword(user.email, user.password)
+      .then(success_callback, failed_callback);
+  };
+}
+
+const firebaseSDK = new FirebaseSDK();
+
+export default firebaseSDK;
+
