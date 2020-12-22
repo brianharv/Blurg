f47235ba182984f8cdf054a464458a3c4be5c2f3
Brian Harvey <brian.harv3y@gmail.com>
2020-12-22 15:34:19 -0800

    add README.md

--- components/Input.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { View, TextInput, ScrollView, StyleSheet, KeyboardAvoidingView } from 'react-native';
-
-const Input = props => {};
-
-const styles = StyleSheet.create({});
-
-export default Input;
